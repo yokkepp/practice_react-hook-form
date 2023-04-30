7dace828bd0cbf3914f5e3c8354cd0fdf132429c
yokkepp <yoseri430@gmail.com>
2023-04-30 22:07:13 +0900

    add pattern

--- src/App.js
+++ src/App.js
@@ -3,19 +3,41 @@ import "./style.css";
 
 export default function App() {
 	/* useFormから関数をimport */
-	const { register, handleSubmit } = useForm();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
 
 	const onSubmit = (data) => console.log("onSubmit:", data);
-
+	console.log("errors:", errors);
 	return (
 		/* handleSubmitはフォームの入力内容を検証したうえで、引数に渡した関数(onSubmit)を実行 */
 		<form onSubmit={handleSubmit(onSubmit)}>
 			<label>姓</label>
-			<input {...register("firstName")} />
+			<input
+				{...register("firstName", {
+					required: true,
+					message: "This is required",
+				})}
+			/>
 			<label>名</label>
-			<input {...register("lastName")} />
+			<input
+				{...register("lastName", {
+					required: true,
+					maxLength: 15,
+				})}
+			/>
+			<label>ニックネーム</label>
+			<input {...register("nickName", { require: true })} />
 			<label>Emailアドレス</label>
-			<input {...register("email")} />
+			<input
+				{...register("email", {
+					require: true,
+					pattern: /^[a-zA-Z0-9]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9]+)/,
+				})}
+			/>
+
 			<input type='submit' />
 		</form>
 	);
